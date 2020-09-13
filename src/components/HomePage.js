21a6564e33d51700fb434f0de35e5575401ea647
unknown <lazaryangarnik@gmail.com>
2020-09-13 14:01:05 -0700

    tu be continued

---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Nav from './nav/Nav'
+
+export default function homePage(){
+    return (
+        <Nav/>
+    )
+}
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function nav (){
+    return (
+        <div className='nav'>
+                <div className='phone'> imbox.ru</div>
+                <div className='link'>fb, vv, dd</div>
+        </div>
+    )
+}
